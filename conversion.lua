7085d5c859b5a66d1ab4122a7597f91b77473533
wintersknight94 <62315647+wintersknight94@users.noreply.github.com>
2022-01-26 10:16:44 -0600

    added drop-in compatibility for old 'nc_steam'
    Forces an alias on old 'nc_steam' nodes to allow this mod to directly replace 'nc_steam' on servers and singleplayer worlds

---
+++ conversion.lua
@@ -0,0 +1,12 @@
+-- LUALOCALS < ---------------------------------------------------------
+local minetest, nodecore
+    = minetest, nodecore
+-- LUALOCALS > ---------------------------------------------------------
+local modname = minetest.get_current_modname()
+------------------------------------------------------------------------
+
+
+minetest.register_alias("nc_steam:steam",					modname.. ":steam")
+minetest.register_alias("nc_steam:steam_dense",					modname.. ":steam_dense")
+
+
